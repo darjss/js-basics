bc176b6cccb838d0258ddc77b3d1eee4175a41bc
darjss <darjsavid@gmail.com>
2023-11-27 12:31:39 +0800

    11.27 class final

--- fff.js
+++ fff.js
@@ -1,5 +1,18 @@
-let x=1001;
+let x=3443;
+let z=0;
+let y;
 while(x>0){
+    y=x%10; //3
+    z=z*10+y; // 30
     x=x/10;
-    
+    Math.round(x)
 }
+if(x==z){
+    console.log("palindrome");
+}
+else{
+    console.log("not palindrome");
+}
+console.log(z);
+console.log(y);
+console.log(x);
--- test.js
+++ test.js
@@ -1,19 +1,103 @@
-let fname = "avidaa";
-let lName = "darjs";
-let myAge = 222;
-console.log("my name is", fname, lName);
-console.log("my age is", myAge);
-let num1 = 9999999999999999999999999999999999999999999999999999999999999999100;
-let num2 = 99999999999999999999999999999999999999999999999999999998101;
-console.log(num1 * num2);
-console.log(num1 / num2);
-console.log(num1 + num2);
-console.log(sum);
-let x=11;
-let y=x*x+x*2+1;
-console.log("the answer of the expression is", y);
-// exercise 1
-let number1=314;
-let number2=128;
-let sum=number1+number2;
-console.log("the sum of the two number is", sum);
+// let fname = "avidaa";
+// let lName = "darjs";
+// console.log("my name is", fname, lName);
+// console.log("my age is", myAge);
+// let num1 = 9999999999999999999999999999999999999999999999999999999999999999100;
+// let num2 = 99999999999999999999999999999999999999999999999999999998101;
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+// console.log(num1 + num2);
+
+// let x=11;
+// let y=x*x+x*2+1;
+// console.log("the answer of the expression is", y);
+// // exercise 1
+// let number1=314;
+// let number2=128;
+// let sum=number1+number2;
+// console.log("the sum of the two number is", sum);
+// let l=40;
+// let k=40;
+// if(l>k){
+//     console.log(" l is greater")
+// }
+// else if(l<k){
+//     console.log("k is greater")
+// }
+// else{
+//     console.log("they are even")
+// }
+// function dateDifference(birthday){
+//     getFullYear(birthday)
+// }
+// let birthday= new Date(2002-11-22);
+// let birthYear=2002;
+// let myAge = 2023-birthYear;
+// if(myAge>=18){
+//     console.log("you can drive")
+// }
+// else{
+//     console.log("you cant drive")
+// }
+// console.log(birthday);
+// console.log("My age=",myAge);
+let day=5;
+if(day==1){
+    console.log("Today is Monday")
+} else if(day==2){
+    console.log("Today is Tuesday")
+} else if(day==3){
+    console.log("Today is Wednesday")
+} else if(day==4){
+    console.log("Today is Thursday")
+} else if(day==5){
+    console.log("Today is Friday")
+} else if(day==6){
+    console.log("Today is Saturday")
+} else if(day==7){
+    console.log("Today is Sunday")
+} else{
+    console.log("Error:The day you entered must be between 1 and 7")
+}
+//identify which month is the day in 
+let x=65
+if(x<=31){
+    console.log("This day is in January")
+} else if(x<=59){
+    console.log("This day is in Febraury")
+} else if(x<=90){
+    console.log("This day is in March")
+} else if(x<=120){
+    console.log("This day is in April")
+} else if(x<=151){
+    console.log("This day is in May")
+} else if(x<=181){
+    console.log("This day is in June")
+} else if(x<=212){
+    console.log("This day is in July")
+} else if(x<=243){
+    console.log("This day is in August")
+} else if(x<=273){
+    console.log("This day is in September")
+} else if(x<=304){
+    console.log("This day is in October")
+} else if(x<=334){
+    console.log("This day is in November")
+} else if(x<=365){
+    console.log("This day is in December")
+} //grade
+let midterm1=80;
+let midterm2=90;
+let final=85;
+let grade= (midterm1+midterm2+final*2)/4
+if(grade<=60){
+    console.log("Your grade is F")
+} else if(grade<=70){
+    console.log("You grade is D")
+} else if(grade<=80){
+    console.log("You grade is C")
+} else if(grade<=90){
+    console.log("You grade is B")
+} else[
+    console.log("Your grade is A")
+]
