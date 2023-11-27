b38dfb8759924c8034b69d934ec8ab38f036e2f1
darjss <darjsavid@gmail.com>
2023-11-27 10:39:19 +0800

    first commit initializing

---
+++ test.js
@@ -0,0 +1,19 @@
+let fname = "avidaa";
+let lName = "darjs";
+let myAge = 222;
+console.log("my name is", fname, lName);
+console.log("my age is", myAge);
+let num1 = 9999999999999999999999999999999999999999999999999999999999999999100;
+let num2 = 99999999999999999999999999999999999999999999999999999998101;
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 + num2);
+console.log(sum);
+let x=11;
+let y=x*x+x*2+1;
+console.log("the answer of the expression is", y);
+// exercise 1
+let number1=314;
+let number2=128;
+let sum=number1+number2;
+console.log("the sum of the two number is", sum);
